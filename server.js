d0b92bbe063c8cbbf8003bcc811b9062717918c5
Bexruz <bexruzxolmuminov@gmail.com>
2020-06-17 23:56:38 +0500

    fsdfsdfafasd

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ if (process.env.NODE_ENV === 'production'){
   //set static folder
   app.use(express.static('client/build'));
 
-  // app.get('*', (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  // });
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 //Connection to database and server

9ba34cfdbdd63ae6a2f7b744c196fdb7b57d722f
Bexruz <bexruzxolmuminov@gmail.com>
2020-06-18 00:45:16 +0500

    dasdasfasdf

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use('/t', require('./routes/redirect.route'));
 //Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))

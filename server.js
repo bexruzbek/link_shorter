eaf519fba3418b7a6924d359262610c60edd158d
Bexruz <bexruzxolmuminov@gmail.com>
2020-06-17 23:03:08 +0500

    Last

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const config = require('config');
+const path = require('path');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -12,8 +13,18 @@ app.use('/api/auth', require('./routes/auth.route'));
 app.use('/api/links', require('./routes/links.route'));
 app.use('/t', require('./routes/redirect.route'));
 
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 //Connection to database and server
-const PORT = config.get('port') || 5000;
+const PORT = process.env.PORT || 5000;
 
 async function start() {
   try {

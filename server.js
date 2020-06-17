640dc34194415756593427f3e4286dbcc949b8e1
Bexruz <bexruzxolmuminov@gmail.com>
2020-06-17 23:55:01 +0500

    dasfsdfa

--- server.js
+++ server.js
@@ -18,13 +18,13 @@ if (process.env.NODE_ENV === 'production'){
   //set static folder
   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // });
 }
 
 //Connection to database and server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 async function start() {
   try {
